4c4ae7804deeeec991ddac2d193cfdf95c51004d
Daniel Barrell <daniel.barrell@eaglegenomics.com>
2016-01-26 13:17:40 +0000

    incremented version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'info@eaglegenomics.com'
 license 'apache2'
 description 'Installs/Configures R2R'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.0'
+version '1.0.0'
 
 depends 'build-essential'
 depends 'magic_shell'
